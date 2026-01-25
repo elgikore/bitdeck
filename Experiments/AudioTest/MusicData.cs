2187c3a18856839a8f89f3982af9245960f94494
elgikore <elgienbertc@gmail.com>
2026-01-25 23:53:19 +1300

    some changes in MusicData

--- Experiments/AudioTest/MusicData.cs
+++ Experiments/AudioTest/MusicData.cs
@@ -10,6 +10,8 @@ public struct MusicData
     public string AlbumArtPath { get; init; }
     public string Year { get; init; }
     public TimeSpan DurationMilliseconds { get; init; }
+    public string SampleRate { get; init; }
+    public string ChannelName { get; init; }
     public string AudioPath { get; init; }
 
     public MusicData(string audioPath)
@@ -18,4 +20,17 @@ public struct MusicData
         metadata.Parse();
         
     }
+    
+    // return channelName switch
+    // {
+    //     ChannelName.Mono => "Mono",
+    //     ChannelName.Stereo => "Stereo",
+    //     ChannelName.Surround2_1 => "2.1 Surround",
+    //     ChannelName.Quad => "Quad",
+    //     ChannelName.Surround5_0 => "5.0 Surround",
+    //     ChannelName.Surround5_1 => "5.1 Surround",
+    //     ChannelName.Surround6_1 => "6.1 Surround",
+    //     ChannelName.Surround7_1 => "7.1 Surround",
+    //     _ => throw new ArgumentOutOfRangeException(nameof(channelName), channelName, "Invalid channel name!")
+    // };
 }
