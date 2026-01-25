a36d561c6e3384e5a2697982b90d44f60dd1cc48
elgikore <elgienbertc@gmail.com>
2026-01-25 22:20:54 +1300

    libvlc is a bit differnt from naudio huh

--- Experiments/AudioTest/Program.cs
+++ Experiments/AudioTest/Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+using System.Runtime.InteropServices.ComTypes;
+using LibVLCSharp.Shared;
+
+Core.Initialize();
+var libVlc = new LibVLC();
+var mediaPlayer = new MediaPlayer(libVlc); 
+
+// Reference: \MusicPlayer\BitDeck\Experiments\AudioTest\bin\Debug\net9.0
+string audioPath = Path.GetFullPath("../../../../../../input.wav");
+
+Console.WriteLine(audioPath);
+
+using var metadata = new Media(libVlc, audioPath);
+await metadata.Parse();
+TimeSpan totalTime = TimeSpan.FromMilliseconds(metadata.Duration);
+
+using var media = new Media(libVlc, audioPath);
+mediaPlayer.Play(media);
+
+
+mediaPlayer.TimeChanged += (_, e) =>
+{
+    TimeSpan elapsedTime = TimeSpan.FromMilliseconds(e.Time);
+    Console.Write($"\r{elapsedTime:mm\\:ss}/{totalTime:mm\\:ss}");
+};
+
+Console.ReadKey();
