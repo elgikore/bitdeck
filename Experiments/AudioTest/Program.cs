5aec03cac81922284e01ffb6f95f21ea934b8636
elgikore <elgienbertc@gmail.com>
2026-01-26 00:03:14 +1300

    experimented with how to get samp. rate and channels

--- Experiments/AudioTest/Program.cs
+++ Experiments/AudioTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Immutable;
 using AudioTest;
 using LibVLCSharp.Shared;
 
@@ -16,16 +17,19 @@ using var metadata = new Media(libVlc, audioPath);
 await metadata.Parse();
 TimeSpan totalTime = TimeSpan.FromMilliseconds(metadata.Duration);
 
-using var media = new Media(libVlc, audioPath);
-mediaPlayer.Play(media);
+Console.WriteLine(metadata.Tracks[0].Data.Audio.Channels);
+Console.WriteLine(metadata.Tracks[0].Data.Audio.Rate);
 
-
-mediaPlayer.TimeChanged += (_, e) =>
-{
-    TimeSpan elapsedTime = TimeSpan.FromMilliseconds(e.Time);
-    Console.Write($"\r{elapsedTime:mm\\:ss}/{totalTime:mm\\:ss}");
-};
-
-mediaPlayer.EndReached += (_, _) => Environment.Exit(0);
-
-Console.ReadKey(); // Need or else it quits instantly
+// using var media = new Media(libVlc, audioPath);
+// mediaPlayer.Play(media);
+//
+//
+// mediaPlayer.TimeChanged += (_, e) =>
+// {
+//     TimeSpan elapsedTime = TimeSpan.FromMilliseconds(e.Time);
+//     Console.Write($"\r{elapsedTime:mm\\:ss}/{totalTime:mm\\:ss}");
+// };
+//
+// mediaPlayer.EndReached += (_, _) => Environment.Exit(0);
+//
+// Console.ReadKey(); // Need or else it quits instantly
