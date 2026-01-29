65a95f111e7d7428af69d1213b81b648b174ad47
elgikore <elgienbertc@gmail.com>
2026-01-30 01:06:23 +1300

    some startup code

--- Experiments/AudioVisualizerTest/MainWindow.axaml.cs
+++ Experiments/AudioVisualizerTest/MainWindow.axaml.cs
@@ -1,11 +1,32 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using LibVLCSharp.Shared;
 
 namespace AudioVisualizerTest;
 
 public partial class MainWindow : Window
 {
+    private readonly MediaPlayer _mainMediaPlayer;
+    private readonly MediaPlayer _visualizerMediaPlayer;
+    private readonly LibVLC _libVlcInstance;
+    private readonly string _audioPath = Path.GetFullPath("../../../../../../input2Copy.wav");
+
     public MainWindow()
     {
         InitializeComponent();
+        Core.Initialize();
+        
+        _libVlcInstance = new LibVLC();
+
+        _mainMediaPlayer = new MediaPlayer(_libVlcInstance);
+        _visualizerMediaPlayer = new MediaPlayer(_libVlcInstance);
+        
+        // _visualizerMediaPlayer.SetAudioFormatCallback();
+    }
+    
+    private void PlayButton_Click(object? sender, RoutedEventArgs e)
+    {
+        using var media = new Media(_libVlcInstance, _audioPath);
+        _visualizerMediaPlayer.Play(media);
     }
 }
