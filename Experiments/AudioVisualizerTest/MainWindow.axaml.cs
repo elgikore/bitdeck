c9ea71b831b9e15c6992117775fa4816f3f085c2
elgikore <elgienbertc@gmail.com>
2026-01-30 01:45:13 +1300

    set the audio format for visualization

--- Experiments/AudioVisualizerTest/MainWindow.axaml.cs
+++ Experiments/AudioVisualizerTest/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using LibVLCSharp.Shared;
@@ -21,9 +22,36 @@ public partial class MainWindow : Window
         _mainMediaPlayer = new MediaPlayer(_libVlcInstance);
         _visualizerMediaPlayer = new MediaPlayer(_libVlcInstance);
         
-        // _visualizerMediaPlayer.SetAudioFormatCallback();
+        _visualizerMediaPlayer.SetAudioFormatCallback(
+            (ref IntPtr _, ref IntPtr format, ref uint rate, ref uint channels) =>
+            {
+                format = Marshal.StringToHGlobalAnsi("FL32");
+                rate = 48000;
+                channels = 1;
+
+                return 0;
+            },
+            
+            opaque =>
+            {
+                if (opaque == IntPtr.Zero) return;
+                
+                Marshal.FreeHGlobal(opaque);
+            } 
+        );
+        
+        
     }
-    
+
+    protected override void OnClosed(EventArgs e)
+    {
+        _mainMediaPlayer.Dispose();
+        _visualizerMediaPlayer.Dispose();
+        _libVlcInstance.Dispose();
+        
+        base.OnClosed(e);
+    }
+
     private void PlayButton_Click(object? sender, RoutedEventArgs e)
     {
         using var media = new Media(_libVlcInstance, _audioPath);
