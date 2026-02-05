980c48ddb4105c4330fee58b58dc305279434ef6
elgikore <elgienbertc@gmail.com>
2026-02-06 04:20:38 +1300

    testing fft

---
+++ FftTest/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Default initialization (48kHz, stereo, 512 frames)
+
+using Ownaudio.Core;
+using OwnaudioNET;
+using OwnaudioNET.Sources;
+
+OwnaudioNet.Initialize();
+
+// Custom configuration
+var config = new AudioConfig
+{
+    SampleRate = 44100,
+    Channels = 2,
+    BufferSize = 256
+};
+OwnaudioNet.Initialize(config);
+
+string audioPath = Path.GetFullPath("../../../../../input3.mp3");
+var source = new FileSource(audioPath);
+
+float[] audioData = source.GetFloatAudioData(TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(25));
+int nSize = audioData.Length;
+
+// 
+
+
+Console.WriteLine(audioData.Length);
+
+
+
+
+// Shutdown
+OwnaudioNet.Shutdown();
