f227c62f8fd1e64e17c440d635a20a13dbf17dfa
elgikore <elgienbertc@gmail.com>
2026-02-28 14:15:28 +1300

    switched to rms

--- Experiments/AudioVisualizerTestSecond/MainWindow.axaml.cs
+++ Experiments/AudioVisualizerTestSecond/MainWindow.axaml.cs
@@ -32,11 +32,11 @@ public partial class MainWindow : Window
             using var device = engine.InitializePlaybackDevice(null, audioFormat);
             using var dataProvider = new StreamDataProvider(engine, new FileStream(audioPath, FileMode.Open));
             var player = new SoundPlayer(engine, device.Format, dataProvider);
-            
-            var spectrumAnalyzer = new SpectrumAnalyzer(audioFormat, fftSize: 2048);
+
+            var levelAnalyzer = new LevelMeterAnalyzer(audioFormat);
 
             // Attach the analyzer to the player.
-            player.AddAnalyzer(spectrumAnalyzer);
+            player.AddAnalyzer(levelAnalyzer);
             
             device.MasterMixer.AddComponent(player);
             device.Start();
@@ -45,23 +45,8 @@ public partial class MainWindow : Window
             var timer = new System.Timers.Timer(100f);
             timer.Elapsed += (_, _) =>
             {
-                // Get the spectrum data from the analyzer.
-                var spectrumData = spectrumAnalyzer.SpectrumData;
-
-                // Print the magnitude of the first few frequency bins.
-                if (spectrumData.Length <= 0) return;
-                
-                float sum = 0;
-                int n = 0;
-                
-                for (int i = 6; i < 65; i++)
-                {
-                    sum += spectrumData[i];
-                    n++;
-                }
-                
-                
-                GlRenderer.CurrentRmsLevel = MathF.Floor(sum / n);
+                GlRenderer.CurrentRmsLevel = levelAnalyzer.Rms;
+                Console.WriteLine(GlRenderer.CurrentRmsLevel);
             };
             
 
