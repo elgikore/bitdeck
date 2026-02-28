9b06bf71a9f614338ff2a38e5948401f193b9168
elgikore <elgienbertc@gmail.com>
2026-02-28 14:18:22 +1300

    changed song

--- Experiments/AudioVisualizerTestSecond/MainWindow.axaml.cs
+++ Experiments/AudioVisualizerTestSecond/MainWindow.axaml.cs
@@ -24,7 +24,7 @@ public partial class MainWindow : Window
     {
         await Task.Run(() =>
         {
-            string audioPath = Path.GetFullPath("../../../input2Copy.wav");
+            string audioPath = Path.GetFullPath("../../../../../../input2Copy.wav");
                 
             using var engine = new MiniAudioEngine();
             var audioFormat = new AudioFormat() { Channels = 2, Format = SampleFormat.S16, SampleRate = 96000 };
