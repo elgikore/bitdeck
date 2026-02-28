76f942763eb02f35a04a59ff1031b1437d952b9a
elgikore <elgienbertc@gmail.com>
2026-02-28 13:59:19 +1300

    after finally learning about opengl, shadertoy, and shaders in general, i am back

---
+++ Experiments/AudioVisualizerTestSecond/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AudioVisualizerTestSecond;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
