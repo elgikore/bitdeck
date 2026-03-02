c215fb518afca320372b0b12d43192f382a85c7d
elgikore <elgienbertc@gmail.com>
2026-03-02 18:11:09 +1300

    ensured that no other code can create the record

--- Experiments/XPlatformAudioTest/AudioMetadata.cs
+++ Experiments/XPlatformAudioTest/AudioMetadata.cs
@@ -11,4 +11,24 @@ public record AudioMetadata
     public required uint SampleRate { get; init; }
     public required uint ChannelCount { get; init; }
     public required TimeSpan DurationSeconds { get; init; }
+
+    // This record class is treated as a Context Pattern
+    // No one can create this record other than AudioMetadata.Parse()
+    #region PrivateConstructor
+    private AudioMetadata(string fileLocation, string title, string artist, string albumName, string albumArtLocation, 
+        string year, uint sampleRate, uint channelCount, TimeSpan durationSeconds)
+    {
+        FileLocation = fileLocation;
+        Title = title;
+        Artist = artist;
+        AlbumName = albumName;
+        AlbumArtLocation = albumArtLocation;
+        Year = year;
+        SampleRate = sampleRate;
+        ChannelCount = channelCount;
+        DurationSeconds = durationSeconds;
+    }
+    #endregion 
+    
+    
 }
