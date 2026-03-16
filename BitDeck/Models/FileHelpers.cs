9d6478e4f41518a8d02c8a437b967da2c7490d22
elgikore <elgienbertc@gmail.com>
2026-03-16 18:37:22 +1300

    resolve directories and user path

--- BitDeck/Models/FileHelpers.cs
+++ BitDeck/Models/FileHelpers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 
 namespace BitDeck.Models;
@@ -6,10 +7,20 @@ public static class FileHelpers
 {
     public static string ResolveToAbsolutePathAndCheck(string fileLocation)
     {
+        // If ~
+        if (fileLocation.StartsWith('~'))
+        {
+            string userLocation = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            
+            fileLocation = userLocation + Path.DirectorySeparatorChar + fileLocation[1..];
+        }
+        
         fileLocation = Path.GetFullPath(fileLocation);
         
         return !File.Exists(fileLocation) ? 
-            throw new FileNotFoundException("File not found", fileLocation) : 
+            (Directory.Exists(fileLocation) ? 
+                throw new NotSupportedException("This is a directory!") : 
+                throw new FileNotFoundException("File not found.", fileLocation) ) : 
             fileLocation;
     }
 }
